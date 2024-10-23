a43d9fa790eb9bcf69b922523059c50ce75f2c2a
Aj Gibbs <ajgibbs16@gmail.com>
2024-10-22 20:38:49 -0400

    test commiit aj

---
+++ src/main/java/newtest/tryThis.java
@@ -0,0 +1,12 @@
+package newtest;
+
+public class tryThis {
+    public static void main(String[] args){
+        String aj = "Alphonza";
+        for(int i = 0; i != 8; ++i){
+            System.out.print(aj.charAt(i)+".");
+        }
+
+
+    }
+}
