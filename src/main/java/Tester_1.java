c877993c5315e967f6e500259cda27855923c665
Aj Gibbs <ajgibbs16@gmail.com>
2024-10-22 21:03:42 -0400

    aj push test2

--- src/main/java/Tester_1.java
+++ src/main/java/Tester_1.java
@@ -1,5 +1,9 @@
 public class Tester_1 {
     public static void main (String [] args){
         System.out.println("Testing");
+        //write a code that asks user for width and height and returns area
+
+
+
     }
 }
